dce3c373d5090f56e0ada32a7b8796f6e3bd549a
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2024-07-25 07:42:01 -0400

    Use .NET hosting for DI. (#3)

---
+++ AdrenalineGamesEditor/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MrCapitalQ.AdrenalineGamesEditor;
+
+[ExcludeFromCodeCoverage]
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+
+        builder.Services.AddHostedService<WindowsAppHostedService<App>>();
+
+        builder.Services.AddSingleton<App>();
+        builder.Services.AddSingleton<MainWindow>();
+
+        var host = builder.Build();
+
+        host.Run();
+    }
+}
